2f754bed10104a8d6a433e11d5a893ab9c4ea590
KlasshDEV <klasshdev@gmail.com>
2022-08-01 18:18:57 -0600

    Started portfolio project and organized folder

---
+++ MedicalProject/medical-insurance-costs.py
@@ -0,0 +1,21 @@
+# perameters of dataset [age,sex,bmi,children,smoker,region,charges]
+# int: age, bmi, children, charges(float)
+# str: smoker(yes, no), region
+
+# Questions, what are major factors to cost
+# Whats the average cost
+# What's the average age of participents
+# where are the majority from
+# what is the average age of someone with atleast 1 child
+# What what's the average cost per region, sex, age?
+# Smoker make a significant difference?
+# BMI make a significant difference?
+# Can you use this data to accurately predict insurance costs
+
+# PROCESSING IDEAS
+# Use SQL
+# Make Dictionary
+# Look for NULL
+# Looks for formatting issues
+# Look for outliers
+
